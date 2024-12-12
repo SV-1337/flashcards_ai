c74c595019b2797265b973c09d28b7abd79601e1
SV-1337 <shubh.varshney08@gmail.com>
2024-12-11 20:50:39 -0600

    Update route.js to clarify start and stop points

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -4,9 +4,9 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 const systemPrompt = `
 You are a flashcard creator, you take in text and create multiple flashcards from it. Make sure to create exactly 12 flashcards.
 Both front and back should be one sentence long. The front should be a question/word and the back should define/explain it.
-You should return in the following format (That will be parsed into JSON, so you do not want to add any characters such as \`:
+You should return in the following format beginning after **START** and ending before **END**. Do not add any other characters other than the format:
 
-
+**START**
 {
   "flashcards":[
     {
@@ -15,6 +15,7 @@ You should return in the following format (That will be parsed into JSON, so you
     }
   ]
 }
+**END**
 `
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
@@ -27,6 +28,7 @@ export async function POST(req){
     const data = await req.text()
     const result = await model.generateContent(data);
 
+    console.log(result.response.text())
     const flashcards = JSON.parse(result.response.text())
 
     return NextResponse.json(flashcards.flashcards)
