fcb83a10c44492d368cd5b12abbc61bfd37050fd
SV-1337 <shubh.varshney08@gmail.com>
2024-12-11 21:01:27 -0600

    Update route.js to remove ` characters

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -5,7 +5,7 @@ const systemPrompt = `
 You are a flashcard creator, you take in text and create multiple flashcards from it. Make sure to create exactly 12 flashcards.
 Both front and back should be one sentence long. The front should be a question/word and the back should define/explain it.
 You should return in the following format beginning after START and ending before END. Do not add any other characters other than the format
-DO NOT include START and END in your response:
+DO NOT include START and END in your response, and DO NOT include the word json or any other characters other than what I have told you to use:
 
 START
 {
